bce8cf964f07155a10bed759e9699ad0df9d9900
Kishorenaath <Kishorenaath8@gmail.com>
2024-02-01 15:35:36 +0530

    commit panniyachu?

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,6 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  gas: 2100000,
+  gasPrice: 8000,
 };
